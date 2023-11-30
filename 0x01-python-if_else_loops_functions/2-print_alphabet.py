630e080a3186cd3f5769e840e444d87be0b7e96e
Yousraaadel <yousraaadel@gmail.com>
2023-11-30 05:49:25 -0800

    h

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def main():
-    for i in range(ord('a'), ord('z') + 1):
-        print(chr(i), end="")
-
-
-
-
-main()
+for i in range(ord('a'), ord('z') + 1):
+    print(chr(i), end="")
