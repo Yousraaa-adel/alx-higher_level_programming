61ebebf0e0885196306582bee299cf5bc6eff07c
Yousraaadel <yousraaadel@gmail.com>
2023-12-01 16:41:05 +0200

    checking

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,13 +1,5 @@
-#!/usr/bin/python3
-def main():
-    char = input("Character: ")
-    result = islower(char)
-    print(result)
-
 def islower(c):
     for i in range(ord("a"), ord("z") + 1):
         if c == chr(i):
             return True
     return False
-
-main()
