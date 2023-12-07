e4ab6cb42c944b53c5903b887d6509e4946eea14
Yousraaadel <yousraaadel@gmail.com>
2023-12-07 20:45:36 +0200

    Nicely done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     # Getting the number of the argv
     arg_len = len(sys.argv) - 1
-
     # arguments if plural and argument when more than 1 args
     arg = "argument" if arg_len == 0 or arg_len == 1 else "arguments"
 
     pun = "." if arg_len < 1 else ":"
 
     line = "\n" if arg_len >= 1 else ""
-        
+
     # Output for the number of args + argument
     output = "{} {}{}{}".format(arg_len, arg, pun, line)
 
