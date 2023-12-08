460d8e4efab174d8ca73ffd1db0ff77280bb0a3d
Yousraaadel <yousraaadel@gmail.com>
2023-12-08 18:08:13 +0200

    Uploading

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    if sys.argv == 1:
+        print("0")
+    # Excluding the fille name
+    args = sys.argv[1:]
+    # map iterates over the args and float turning them into float numbers and then sum adds them
+    result = sum(map(float, args))
+    # Reternning the numbers to integers
+    result = int(result)
+    # Final output print
+    print("{}".format(result))
