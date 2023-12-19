aee311fb8a6abd403eb9abfbe42edd90d2e508c0
Yousraaadel <yousraaadel@gmail.com>
2023-12-19 20:31:40 +0200

    fourth

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,7 +17,7 @@ class Square:
             ValueError: If size is less than 0.
         """
 
-        if not isinstance(self, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("Size must be >= 0")
