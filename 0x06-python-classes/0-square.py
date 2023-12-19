d647d9cdbe2c8fcfc4379a19406ec779e0c0a6a9
Yousraaadel <yousraaadel@gmail.com>
2023-12-19 19:01:19 +0200

    class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
-""" This is an empty class """
+""" This class demonstrates an empty class
+    the pass statement is meant to not recieve errors for not
+    leaving anything inside the class
+
+    the newline at the end of the file is a project requirement
+"""
+
 
 class Square:
     pass
