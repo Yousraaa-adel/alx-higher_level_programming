9338fdfe7e700a0ef9f28bf573a90aeccd6e2835
Yousraaadel <yousraaadel@gmail.com>
2023-12-21 19:50:38 +0200

    fifth task - removing letter from string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 
 def no_c(my_string):
     result = ""
-    for i in my_string:
-        if i != 'c' or 'C':
-            result += i
+    for i in range(len(my_string)):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            result += my_string[i]
     return result
