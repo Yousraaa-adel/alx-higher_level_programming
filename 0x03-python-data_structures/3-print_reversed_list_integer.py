986a226db062c368ca9551a298882c2a7d79cbc0
Yousraaadel <yousraaadel@gmail.com>
2023-12-21 19:30:24 +0200

    third task - list in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,6 +5,6 @@ def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return None
 
-    my_list.reverse()
-    if i in my_list:
+    reversedList =  list.reverse(my_list)
+    if i in reversedList:
         print("{:d}".format(i))
