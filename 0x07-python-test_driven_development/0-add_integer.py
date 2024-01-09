47a7c9c7e0eac06ea01c373e6092cd2cb0e92dbe
Yousraaadel <yousraaadel@gmail.com>
2024-01-09 17:17:59 +0200

    test-driven

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -23,10 +23,10 @@ def add_integer(a, b=98):
     if a is None:
         raise TypeError("add_integer() missing 1 required positional argument: 'a'")
 
-    if b == 98: 
+    if b == 98:  # Check if b has its default value
         raise TypeError("add_integer() missing 1 required positional argument: 'b'")
 
-     /*Checking that a, b are of type integers or floats only*/
+    # Checking that a, b are of type integers or floats only
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
     
@@ -34,12 +34,12 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer or b must be an integer")
 
 
-
+    # Casting a, b to integers if they are floats
     if isinstance(a, float):
         a = int(a)
 
     if isinstance(b, float):
         b = int(b)
- 
+    # Perform addition and return result
     result = a + b
     return result
