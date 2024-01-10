911de078fe183ae5297866baa7135446e347d0c4
Yousraaadel <yousraaadel@gmail.com>
2024-01-10 20:00:23 +0200

    trying this test

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    This is the "3-say_my_name" module
+
+    The 3-say_my_name module divides all elements of a matrix.
+"""
+def say_my_name(first_name, last_name=""):
+    """divides all elements of a matrix.
+
+    Args:
+        first_name (str): First value
+        last_name (str): Second value
+
+    Raises:
+        TypeError: If first_name or las_name are NOT of type str
+
+    Returns:
+        str: My name is <first name> <last name>
+    """
+
+    if not isinstance(first_name, str) or isinstance(first_name, bool):
+        raise TypeError("first_name must be a string")
+    
+    # Print the full name with last_name if provided, otherwise just first_name
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
+    
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    
