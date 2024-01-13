e7b1fc083cec1d25fd61e32d28fcd171d5a7d5ba
Yousraaadel <yousraaadel@gmail.com>
2024-01-13 17:50:02 +0200

    print_square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+    This is the "4-print_square" module
+
+    The 4-print_square module divides all elements of a matrix.
+"""
+def print_square(size):
+    """ Prints a square with the character #.
+
+    Args:
+        size (int): First value
+
+    Raises:
+        TypeError: If size is NOT of type int
+        TypeError: If size is a float and is less than 0
+        ValueError: If size is less than 0
+
+    Returns:
+        str: prints a square with the character #.
+    """
+
+    if not isinstance(size, int) or isinstance(size, bool):
+        raise TypeError("size must be an integer")
+    
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    
+    for row in range(size):
+        print("#", end="")
+        for col in range(1, size):
+            print("#", end="")
+        print("")
