8244c0e5c6b36f3bcd0f8f152a4ed824b8e1c67e
Yousraaadel <yousraaadel@gmail.com>
2024-01-13 18:49:28 +0200

    text_indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    This is the "4-print_square" module
+
+    The 5-text_indentation divides all elements of a matrix.
+"""
+def text_indentation(text):
+    """ Prints a square with the character #.
+
+    Args:
+        size (int): Size of the square's side length.
+
+    Raises:
+        TypeError: If size is NOT of type int
+        TypeError: If size is a float and is less than 0
+        ValueError: If size is less than 0
+
+    Returns:
+        str: prints a square with the character #.
+    """
+
+    if not isinstance(text, str) or isinstance(text, bool):
+        raise TypeError("text must be a string")
+    
+    newText = ""
+
+    for c in text:
+        if c in ['.', '?', ':']:
+            newText += c + "\n\n"
+        else:
+            newText += c
+        
+    print(newText)
