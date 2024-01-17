789461814426ad0617f64ca1f50d93b86692d8cc
Yousraaadel <yousraaadel@gmail.com>
2024-01-17 20:40:50 +0200

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" A Module that has the Student Class """
+
+
+class Student:
+    """
+    Class that defines properties of student.
+
+    Attributes:
+        first_name (str): first name of student
+        last_name (str): last name of student
+        age (int): age of the student
+    """
+    def __init__(self, first_name, last_name, age):
+        """Creates new instances of Student.
+
+        Args:
+            first_name (str): first name of student
+            last_name (str): last name of student
+            age (int): age of the student
+        """
+        first_name = self.first_name
+        last_name = self.last_name
+        age = self.age
+
+    def to_json(self):
+        """retrieves a dictionary representation of
+        a Student instance
+
+        Returns:
+            dict: dictionary.
+        """
+        return self.__dict__
