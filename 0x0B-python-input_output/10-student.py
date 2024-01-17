70346c308b02f95d4bcd637cea68460e7da1137f
Yousraaadel <yousraaadel@gmail.com>
2024-01-17 20:53:40 +0200

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" A Module that has the Student Class """
+"""Module defining the class Student based on 9-student.py"""
 
 
 class Student:
@@ -7,37 +7,39 @@ class Student:
     Class that defines properties of student.
 
     Attributes:
-        first_name (str): first name of student
-        last_name (str): last name of student
-        age (int): age of the student
+        first_name (str): first name of student.
+        last_name (int): last name of student.
+        age (int): age of student.
     """
     def __init__(self, first_name, last_name, age):
         """Creates new instances of Student.
 
         Args:
-            first_name (str): first name of student
-            last_name (str): last name of student
-            age (int): age of the student
+            first_name (str): first name of student.
+            last_name (int): last name of student.
+            age (int): age of student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of
-        a Student instance
+        """Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attribute names contained in,
+        this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
 
         Returns:
-            dict: dictionary.
+            dict: dictionary representation.
         """
         if attrs is None:
             return self.__dict__
 
-        new_dict = []
+        new_dict = {}
         for item in attrs:
             try:
                 new_dict[item] = self.__dict__[item]
             except Exception:
                 pass
-
-            return new_dict
+        return new_dict
