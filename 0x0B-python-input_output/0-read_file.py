f545e97248f880433085f0e7d838cc5262de62e6
Yousraaadel <yousraaadel@gmail.com>
2024-01-17 16:49:26 +0200

    input/output[

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+""" A Module contaiing a function called read_file """
 
 
 def read_file(filename=""):
+    """ Reads a file and prints to stdout
+
+    Args:
+        filename (str, optional): name of the file to read.
+        Defaults to ""
+    """
     with open(filename, "r", encoding="UTF8") as file:
         content = file.read()
         print(content)
