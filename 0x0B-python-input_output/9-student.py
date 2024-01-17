f05731e45acce14980a70cbb9a68c2f1ac45f712
Yousraaadel <yousraaadel@gmail.com>
2024-01-17 20:43:18 +0200

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -20,7 +20,7 @@ class Student:
             age (int): age of the student
         """
         self.first_name = first_name
-        self.last_name = last_name =
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
