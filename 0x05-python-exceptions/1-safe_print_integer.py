bdbb66fb8cdcef0ce0aa59d8ff85efac3b406c05
Yousraaadel <yousraaadel@gmail.com>
2024-01-22 17:58:14 +0200

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -21,7 +21,7 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value))
-        print()
+        print("")
         return True
     except (ValueError, TypeError):
         return False
