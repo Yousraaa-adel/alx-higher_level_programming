416553561272d699af5c78842630862e8e7516c2
Yousraaadel <yousraaadel@gmail.com>
2024-01-22 17:45:53 +0200

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -16,13 +16,17 @@ def safe_print_list(my_list=[], x=0):
     Raises:
         IndexError: when x is bigger than my_list
     """
+    count = 0
+
     for i in range(x):
         try:
             print(my_list[i], end="")
+            count += 1
         except IndexError:
             break
 
     print()
+    return count
 
 
 if __name__ == "__main__":
