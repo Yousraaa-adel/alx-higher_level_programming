b1ffb1a8e49cdb88f8ec6995a8ad15d45d4c57dd
Yousraaadel <yousraaadel@gmail.com>
2024-01-22 18:26:56 +0200

    task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    This is the "3-safe_print_division" module
+
+    The 3-safe_print_division module supplies one function.
+"""
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides 2 integers and prints the result.
+
+    Args:
+        a (int): first value
+        b (int): second value
+
+    Raises:
+        ValueError and TypeError
+    """
+    div = 0
+    tmp_result = 0
+
+    for i in range(0, list_length):
+        try:
+            my_list_1[i] / my_list_2[i]
+        except TypeError:
+            tmp_result = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            tmp_result = 0
+            print("division by 0")
+        except IndexError:
+            tmp_result = 0
+            print("out of range")
+        finally:
+            pass
+
+        div.append(tmp_result)
+    return div
