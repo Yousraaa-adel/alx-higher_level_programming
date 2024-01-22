2ed4f1f9916583462211d072ef4191a351c1e40c
Yousraaadel <yousraaadel@gmail.com>
2024-01-22 17:43:13 +0200

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    This is the "0-safe_print_list" module
+
+    The 0-safe_print_list module supplies one function.
+"""
+
+
+def safe_print_list(my_list=[], x=0):
+    """Prints x elements of a list.
+
+    Args:
+        my_list (list): the list with items to be printed
+        x (int): num of items to be printed from my_list. Defaults to 0.
+
+    Raises:
+        IndexError: when x is bigger than my_list
+    """
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+        except IndexError:
+            break
+
+    print()
+
+
+if __name__ == "__main__":
+    main()
