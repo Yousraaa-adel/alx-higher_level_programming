45e0e577bcc670c3c574b6de99a2139505aa9382
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-02-15 17:33:53 +0200

    task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- Script to print the full description of the table first_table from the database hbtn_0c_0
-SHOW COLUMNS FROM hbtn_0c_0.first_table;
+-- Prints full description of first_table
+SHOW CREATE table first_table;
