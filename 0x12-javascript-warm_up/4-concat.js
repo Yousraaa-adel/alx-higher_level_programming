936e085ca9a31a66b4108638503a5f410de1bd9b
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-03-13 16:06:05 +0200

    js 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
-const arg1 = args[2] || undefined;
-const arg2 = args[3] || undefined;
+const arg1 = args[2] || 'undefined';
+const arg2 = args[3] || 'undefined';
 
 if (args === 2) {
   console.log(arg1 + ' is ' + arg2);
