b724c2c5fc913dc4da248bece0c295fdfe079408
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-03-16 16:38:17 +0200

    js 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,10 +3,6 @@ const args = process.argv;
 const num1 = parseInt(args[2]);
 const num2 = parseInt(args[3]);
 
-if (isNaN(num1) || isNaN(num2) || !num1 || !num2) {
-  console.log('NaN');
-}
-
 function add (num1, num2) {
   return num1 + num2;
 }
