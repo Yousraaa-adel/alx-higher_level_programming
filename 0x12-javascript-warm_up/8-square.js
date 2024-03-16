a4d0e841dd68a1b2ae02cb78794ce1ab45170e8b
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-03-16 16:19:46 +0200

    8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const args = process.argv;
+const size = parseInt(args[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < i; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
