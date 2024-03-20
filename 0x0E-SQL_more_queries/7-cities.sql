cc2450519851e9f11217159abcccd7218f688dac
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-03-20 23:06:44 +0200

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -6,6 +6,5 @@ USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `cities` (
   `id` INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
   `name` VARCHAR(256) NOT NULL,
-  FOREIGN KEY (`state_id`) INT NOT NULL 
-  REFERENCES `states` (`id`)
+  FOREIGN KEY (`state_id`) REFERENCES `states` (`id`)
 );
