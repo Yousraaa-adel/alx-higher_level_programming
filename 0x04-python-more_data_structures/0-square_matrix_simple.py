be952e19637287654d2662946dc3d2f8c52faccd
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-18 17:52:37 +0200

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
 def square_matrix_simple(matrix=[]):
-  [[row[i] for row in matrix] for i in range(0, 3)]
+  result = []
+  new_row = []
+
+  for row in matrix:
+    new_row = []
+    for value in row:
+        
