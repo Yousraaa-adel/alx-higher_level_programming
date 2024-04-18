6d1e53dd746d0b7f5cb41cdda6ff9898449608a2
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-18 17:53:11 +0200

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -13,4 +13,7 @@ def square_matrix_simple(matrix=[]):
   for row in matrix:
     new_row = []
     for value in row:
-        
+        new_row.append(value ** 2)
+    result.append(new_row)
+
+  return result
