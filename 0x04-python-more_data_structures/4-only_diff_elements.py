f7c413f5ae56ace787ccdd8b84d025837dd84263
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-18 18:54:02 +0200

    task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    return set_1.union(set_2)
+    new_list = []
+
+    for i in set_1:
+        new_list.append(i)
+
+    for j in set_2:
+        new_list.append(j)
+
+    return new_list
