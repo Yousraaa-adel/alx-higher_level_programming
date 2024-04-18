9d0423283c9eea3cdeea2fe383a7b262d08fce5a
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-18 17:43:03 +0200

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+  [[row[i] for row in matrix] for i in range(0, 3)]
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+More on data structure with Python on: Tuples, Sets, Dicinories.
