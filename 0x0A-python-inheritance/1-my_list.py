8ccc6292ad065a2a781e31fbc9d59afc8e119df3
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-22 17:19:47 +0200

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Defines a class MyList that inherits from list """
+
+
+class MyList(list):
+    """
+        Returns a new list sorted in an ascending order
+    """
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list)
