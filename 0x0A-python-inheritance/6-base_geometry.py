92f6c51d4ed5e311646b66211ad6ea10e9561d00
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-22 18:25:24 +0200

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines an empty class called BaseGeometry. """
+
+
+class BaseGeometry():
+    """ Represents base BaseGeometry. """
+
+    def area(self):
+        """ Raises an Exception with the message area() is not implemented. """
+        raise Exception("area() is not implemented")
