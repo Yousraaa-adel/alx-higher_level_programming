183525d7cfffb8cd4fcb1418d297d88c99a67174
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-22 18:16:31 +0200

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines a class that checks a function. """
+
+
+def inherits_from(obj, a_class):
+    """
+        Returns True if the object is an instance of a class that inherited
+        (directly or indirectly) from the specified class; otherwise False.
+    """
+
+    if issubclass(obj, a_class):
+        return True
+    else:
+        return False
