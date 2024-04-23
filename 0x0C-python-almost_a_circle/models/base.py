4a14517b4110cc1e9456860b7bacd6e75f0e4848
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-23 20:11:58 +0200

    task 1 of almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Defines the base class for all other classes in this project. """
+
+
+class Base():
+    """ Represents the Base class. """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Intitializes the class.
+
+        Args:
+            id (id): The id of the new base.
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
