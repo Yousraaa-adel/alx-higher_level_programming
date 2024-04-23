88770338df5470db9a2f5bbd4afbc86537524464
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-23 20:49:50 +0200

    task 2 in almost a circle project

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Defines the Rectangle class that inherits from Base class. """
+Base = __import__('base.py').Base
+
+
+class Rectangle(Base):
+    """ Representation of the Rectangle class. """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Intitializes a new Rectangle.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int): The x coordinate of the rectangle.
+            y (int): The y coordinate of the rectangle.
+            id (int): The id of the new rectangle.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+        @property
+        def width(self):
+            """ Set/get the width of the rectangle. """
+            return self.__width
+
+        @property
+        def height(self):
+            """ Set/get the height of the rectangle. """
+            return self.__height
+
+        @property
+        def x(self):
+            """ Set/get the x coordinate of the rectangle. """
+            return self.__x
+
+        @property
+        def y(self):
+            """ Set/get the y coordinate of the rectangle. """
+            return self.__y
