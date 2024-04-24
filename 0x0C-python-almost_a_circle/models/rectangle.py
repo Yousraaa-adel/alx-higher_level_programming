47241eca8632fad335d571217e6ccf7e6ef4f4fd
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-24 14:13:22 +0200

    adding the setter properties for the rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,17 +27,51 @@ class Rectangle(Base):
             """ Set/get the width of the rectangle. """
             return self.__width
 
+        @width.setter
+        def width(self, value):
+            if type(value) is not int:
+                raise TypeError("width must be an integer.")
+
+            if value <= 0:
+                raise ValueError("width must be bigger than 0.")
+
+            self.__width = value
+
         @property
         def height(self):
             """ Set/get the height of the rectangle. """
             return self.__height
+
+        @height.setter
+        def height(self, value):
+            if type(value) is not int:
+                raise TypeError("height must be an integer.")
+
+            if value <= 0:
+                raise ValueError("height must be bigger than 0.")
 
         @property
         def x(self):
             """ Set/get the x coordinate of the rectangle. """
             return self.__x
 
+        @x.setter
+        def x(self, value):
+            if type(value) is not int:
+                raise TypeError("x coordinate must be an integer.")
+
+            if value <= 0:
+                raise ValueError("x coordinate must be bigger than 0.")
+
         @property
         def y(self):
             """ Set/get the y coordinate of the rectangle. """
             return self.__y
+
+        @y.setter
+        def y(self, value):
+            if type(value) is not int:
+                raise TypeError("y coordinate must be an integer.")
+
+            if value <= 0:
+                raise ValueError("y coordinate must be bigger than 0.")
