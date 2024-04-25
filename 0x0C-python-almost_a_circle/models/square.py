9c9664eb33cbd2769b62ac5a77e1c1b043134f44
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-25 22:06:53 +0200

    Fixing the documentation of the square class.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,14 +11,14 @@ class Square(Rectangle):
 
         Args:
             size (int): The size of the new square.
-            x (int): The x cordinate of the new square.
-            y (int): The y coordinate of the new square.
-            id (int): the identity of the new square.
+            x (int): The x cordinate of the new square. Default is 0.
+            y (int): The y coordinate of the new square. Default is 0.
+            id (int): the identity of the new square. Default is None.
         """
         super().__init__(size, size, x=0, y=0, id)
 
     def __str__(self):
-        """ Override the __str__ method. """
+        """ "Return a string representation of the square. """
 
         return f"[Square] ({self.id}) {self.x}/{self.y} - " \
             f"{self.width}/{self.height}"
