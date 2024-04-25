13827464f1a8fa52dfe181c18c6305c5d70bd59c
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-25 21:32:20 +0200

    fixing pycodestyle.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,4 +106,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """ Overrides the __str__method. """
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - " \
+            f"{self.width}/{self.height}"
