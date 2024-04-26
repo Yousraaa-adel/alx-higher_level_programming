aeb51da7cd6229f1f4e60d39b61170c11b0fd768
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-26 22:24:59 +0300

    Added half the update method to the square class - args part.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -38,3 +38,29 @@ class Square(Rectangle):
 
         return f"[Square] ({self.id}) {self.x}/{self.y} - " \
             f"{self.width}"
+
+    def update(self, *args, **kwargs):
+        """ Assigns attributes.
+
+        Args (int):
+            1st argument represents be the id attribute
+            2nd argument represents be the size attribute
+            3rd argument represents be the x attribute
+            4th argument represents be the y attribute
+        """
+        if args and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+
+                i += 1
