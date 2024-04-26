232a23a0c6193eef187248fe604b3dd0ca063311
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-26 21:59:56 +0300

    Added tests for getters and setters in square class.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,11 +25,10 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """ Sets the value of the size. """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-
+        # if type(value) is not int:
+        #     raise TypeError("width must be an integer")
+        # if value <= 0:
+        #     raise ValueError("width must be > 0")
         self.__width = value
         self.__height = value
 
