f928fd7c672154bc915e9bfb04b980b013a5e0ad
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-04-27 19:55:11 +0300

    Updated the update method in the square clas to add the kwargs.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -70,7 +70,7 @@ class Square(Rectangle):
         elif kwargs and len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "id":
-                    if value is not None:
+                    if value is None:
                         self.__init__(self.size, self.x, self.y)
                     else:
                         self.id = value
