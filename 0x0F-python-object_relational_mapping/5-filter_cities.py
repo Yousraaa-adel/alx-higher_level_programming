9484f6896e284f3f0d2c9a5edcfe38e2e78c255e
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-05-17 17:19:58 +0300

    Task 5 try

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+"""
+import MySQLdb
+from MySQLdb import connect, Error
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        with connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        ) as connection:
+            state_name = sys.argv[4]
+            db_query = ("SELECT cities.name FROM cities \
+                        JOIN states ON \
+                        states.id = cities.state_id \
+                        WHERE states.name = %s")
+            with connection.cursor() as cursor:
+                cursor.execute(db_query, (state_name, ))
+                rows = cursor.fetchall()
+                if rows is not None:
+                    print(rows)
+
+    except Error as e:
+        print(e)
