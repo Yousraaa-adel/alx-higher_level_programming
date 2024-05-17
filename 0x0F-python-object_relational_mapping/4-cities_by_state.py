fea6a52de46fa3b7f157674c122b9adf813f4b1a
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-05-17 13:58:04 +0300

    updating query

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
             password=sys.argv[2],
             database=sys.argv[3]
         ) as connection:
-            db_query = ("FROM * cities ORDER BY cities.id ASC;")
+            db_query = ("SELECT FROM * cities ORDER BY cities.id ASC;")
             with connection.cursor() as cursor:
                 cursor.execute(db_query)
 
