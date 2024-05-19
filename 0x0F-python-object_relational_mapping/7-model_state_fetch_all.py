08243363a2ddf0a325faa0381a99431f38a3ae50
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-05-19 18:18:42 +0300

    Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
             )
         )
 
-    Base.metadata.create_all()
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
