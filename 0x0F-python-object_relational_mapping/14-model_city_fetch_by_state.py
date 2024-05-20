ad72f1661c6e378a188a311493ff5ef722af2ce6
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-05-20 16:25:11 +0300

    Task 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    A script that prints all City objects from the database.
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from sys import argv
+from model_state import Base, State
+from model_city import Base, City
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]
+        )
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    rows = session.query(City, State).filter(
+        City.state_id == State.id).order_by(
+            City.id).all()
+
+    for city, state in cities:
+        print("{}: ({}) {}".format(
+            state.name, city.id, city.name
+        ))
+
+    session.close()
