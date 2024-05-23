7c3382db2c9ddd3c93a901e2d75a62fc7370cf04
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-05-23 14:17:35 +0300

    adv task 1

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    A script that prints all City objects from the database.
+"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from sys import argv
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]
+        )
+    )
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    california = State(name="California")
+    california.cities = [City(name="San Francisco")]
+    session.add(california)
+    session.commit()
+    session.close()
