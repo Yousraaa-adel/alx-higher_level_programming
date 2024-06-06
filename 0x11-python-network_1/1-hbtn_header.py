848e15229981ff07f0ccb79b6ca459237382d13d
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-06 22:30:50 +0300

    Task 0 python network 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,16 +8,17 @@
 from urllib.request import urlopen
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
 
-url = sys.argv[1]
+    url = sys.argv[1]
 
-try:
-    with urlopen(url) as response:
-        content = response.read()
+    try:
+        with urlopen(url) as response:
+            content = response.read()
 
-        X_Request_Id = response.getheader("X-Request-Id")
-        print(X_Request_Id)
-except Exception as e:
-    print(e)
+            X_Request_Id = response.getheader("X-Request-Id")
+            print(X_Request_Id)
+    except Exception as e:
+        print(e)
