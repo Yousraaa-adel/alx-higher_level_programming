1e6d5acd0a1b7aa9c127d68b10add389d89a88a9
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-07 17:41:28 +0300

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -16,7 +16,9 @@ if __name__ == "__main__":
     payload = {"q": letter}
 
     try:
-        response = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+        response = requests.post(
+            "http://0.0.0.0:5000/search_user", data=payload
+            )
 
         r = response.json()
 
