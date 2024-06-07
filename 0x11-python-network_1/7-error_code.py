8eb7fd07e4f1bac06947535def1f98ec27c52669
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-07 17:20:43 +0300

    Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,7 +4,8 @@
 - sends a POST request to the URL with the email as a parameter,
 - and finally displays the body of the response.
 """
-import requests, sys
+import requests
+import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
