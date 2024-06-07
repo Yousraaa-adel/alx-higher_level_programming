e126a560b5a3d50d241e698f679a442b4cead8b2
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-07 16:31:00 +0300

    Task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A script that:
-- takes in a URL:
+- takes in a URL
 - sends a request to the URL
 - and displays the value of the variable X-Request-Id in the response header.
 """
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(1)
 
-url = sys.argv[1]
+    url = sys.argv[1]
 
-try:
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
 
-    print(response.headers["X-Request-Id"])
-except Exception as e:
-    print(e)
+        print(response.headers["X-Request-Id"])
+    except Exception as e:
+        print(e)
