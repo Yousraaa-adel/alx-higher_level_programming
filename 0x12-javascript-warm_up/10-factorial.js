51baf4fb0a27264d7558a39cf8e1bb251b93089d
Yousraaa-adel <yousraaa.adel@gmail.com>
2024-06-20 19:27:03 +0300

    Js task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
-const args = process.argv;
-let num = args[2];
-
-function factorial(num) {
-    
-}
+function factorial (n) {
+    if (n < 0) {
+      return (-1);
+    }
+    if (n === 0 || isNaN(n)) {
+      return (1);
+    }
+    return (n * factorial(n - 1));
+  }
+  
+  console.log(factorial(Number(process.argv[2])));
